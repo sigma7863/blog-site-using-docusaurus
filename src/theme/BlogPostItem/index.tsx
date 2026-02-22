cda5ead84397283565653b65d3d0e4eb39be3b2e
Sigma <mukae.yuta.0811@gmail.com>
2026-02-22 17:09:27 +0900

    fix: BlogPostItemWrapperでGiscusコンポーネントの表示条件を追加

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -1,5 +1,6 @@
 import React, { type ReactNode } from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
+import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 import GiscusComponent from '@site/src/components/GiscusComponent';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type { WrapperProps } from '@docusaurus/types';
@@ -7,10 +8,12 @@ import type { WrapperProps } from '@docusaurus/types';
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): ReactNode {
+  const { isBlogPostPage } = useBlogPost();
+
   return (
     <>
       <BlogPostItem {...props} />
-      <GiscusComponent />
+      {isBlogPostPage && <GiscusComponent />}
     </>
   );
 }
